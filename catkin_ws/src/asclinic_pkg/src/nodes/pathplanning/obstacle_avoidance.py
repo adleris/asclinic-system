8d1bbd379b5487d05523536a7a3dffb0e96a98fd
Alexander Leris <alexander.leris@gmail.com>
2023-05-02 17:29:14 +1000

    16: Add obstacle avoidance state machine
    This part of the state machine has:
    
    - receiving values from the lidar parser
    - publishing detections onto next node
    - managing locking of the publishing of obstacle depending on distance from the obstacle we're avoiding

---
+++ catkin_ws/src/asclinic_pkg/src/nodes/pathplanning/obstacle_avoidance.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+import rospy
+import numpy as np
+
+from std_msgs.msg import Bool
+from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Pose, Point
+
+
+NAMESPACE = "asc/path"
+NODE_NAME = NAMESPACE + "/" + "obstacle_avoidance"
+
+
+class ObstacleAvoidance:
+    """
+    Set up an obstacle avoidance node.
+
+    # Process
+
+    - If an obstacle is detected by the lidar parser, this node will publish
+    the new detection to rest of the pathplanning system. It will log the
+    position the robot was in when the obstacle was detected.
+    - until we have travelled a certain distance from the obstacle position,
+    do not publish any other detections. This is implemented in the code via
+    the `detection_lock`. 
+    - Once we have travelled this distance, release the `detection_lock`
+
+    # ROS Subscribers
+
+    - current pose
+    - `/asc/sensors/lidarScan` : `LaserScan`
+
+    # ROS Publishers
+
+    - `/asc/path/obstacle_avoidance/detection`: `Bool` 
+    """
+
+    OBSTACLE_AVOIDANCE_LOCK_DISTANCE = 0.5 # metres
+
+    def __init__(self):
+        
+        self.detection_lock: bool = False
+        # The current position
+        self.curr_pos: Point = Point(0,0,0)
+        """
+        `obstacle_pos` tracks the position of the current obstacle we are
+        avoiding. If set to None, there is no obstacle being tracked.
+        If `detection_lock` is False then `obstacle_pos` should be None
+        """
+        self.obstacle_pos: Point = None
+
+        self.detection_publisher = rospy.Publisher(NODE_NAME+"/detection", Bool, queue_size=10)
+        rospy.Subscriber("asc/control/curr_pose", Pose, self.poseReceivedCallback)
+        rospy.Subscriber("asc/sensors/obstacle_scan", Bool, self.obstacleInRangeCallback)
+
+
+    def poseReceivedCallback(self, msg):
+        """
+        Subscriber for pose update. Handles the detection lock/unlock.
+
+        :param msg: {Pose}
+        """
+        self.curr_pos = msg.position
+
+        # we are not in avoidance
+        if ! self.detection_lock:
+            return
+
+        # release the lock if we've travelled far enough
+        # since obstacle pos is checked first, this should hopefully? prevent issues on other threads
+        if (obstacle_point is not None) and (point_distance(obstacle_point, self.pose.position) > self.OBSTACLE_AVOIDANCE_LOCK_DISTANCE):
+            self.obstacle_pos = None
+            self.detection_lock = False
+
+
+    def obstacleInRangeCallback(self, msg):
+        """
+        Subscriber for an obstacle detection.
+
+        :param msg: {Bool}
+
+        Publishes 'True' when an obstacle is observed in range.
+        
+        *Should* only publish once per obstacle, as it will wait for the robot
+        to move out of a radius around the last obstacle before it will consider
+        new obstacles.
+        """
+        if ! msg.data:
+            return
+        
+        # if this obstacle is considered a new detection
+        if ! self.detection_lock:
+            self.detection_publisher.publish(msg)
+            self.obstacle_pos = self.curr_pos
+            self.detection_lock = True
+        else:
+            return
+
+
+def point_distance(p1: Point, p2: Point):
+    """Calculates the distances between two Point objects
+    :param p1: Point 1
+    :param p2: Point 2"""
+    return np.sqrt( (p1.x - p2.x)**2 + (p1.y - p2.y)**2 + (p1.z - p2.z)**2 )
+
+
+if __name__ == '__main__':
+
+    global node_name
+    rospy.init_node(NODE_NAME, anonymous=False)
+    obstacle_avoidance = ObstacleAvoidance()
+    
+    rospy.spin()
