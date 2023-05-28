1295d783a3ad36f098abfffb2fbe0ad770f68456
Will Chester <48420074+wschester@users.noreply.github.com>
2023-05-28 13:00:03 +1000

    change Kalman Filter to include suggestions made by alex

--- catkin_ws/src/asclinic_pkg/src/nodes/Control_Systems/OutterLoop/kalman_filter.py
+++ catkin_ws/src/asclinic_pkg/src/nodes/Control_Systems/OutterLoop/kalman_filter.py
@@ -6,16 +6,10 @@ import rospy
 NAMESPACE = "control"
 NODE_NAME = "kalman_filter"
 
-# Constants for easy access to variables
-X = 0
-Y = 1
-PHI = 2
-
-
 class kalman_filter:
     def __init__(self):
         # TODO could make an initial pos topic that is sub to
-        self.currPose = [0] * 3
+        self.currPose = PoseFloat32(0, 0, 0)
 
         # ROS subs and publihing set up
         self.pose_publisher = rospy.Publisher(f"{NAMESPACE}/sys_pose", PoseFloat32, queue_size=1)
@@ -24,16 +18,12 @@ class kalman_filter:
     def update_pose(self, event):
         # This takes the change to pose and adds them to the current pose to be published
         # TODO Change this to the Message
-        self.currPose[X]     += event.x
-        self.currPose[Y]     += event.y
-        self.currPose[PHI]   += event.phi
+        self.currPose.x    += event.x
+        self.currPose.y     += event.y
+        self.currPose.phi   += event.phi
 
         # Set up and publishing pose
-        current_pose = PoseFloat32()
-        current_pose.x = self.currPose[X]
-        current_pose.y = self.currPose[X]
-        current_pose.phi = self.currPose[PHI]
-        self.pose_publisher.publish(current_pose)
+        self.pose_publisher.publish(self.currPose)
 
 if __name__ == "__main__":
 
